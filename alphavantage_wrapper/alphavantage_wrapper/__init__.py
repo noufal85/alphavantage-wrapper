457356784dbc22bd71c0206118e5104bfe15712f
Noufal E <noufal85@gmail.com>
2025-05-14 20:36:35 -0700

    Add inner package structure

---
+++ alphavantage_wrapper/alphavantage_wrapper/__init__.py
@@ -0,0 +1,8 @@
+"""Alpha Vantage API Wrapper Package.
+
+This package provides a clean, intuitive interface to the Alpha Vantage API.
+"""
+
+from .client import AlphaVantage
+
+__version__ = '0.1.0'
